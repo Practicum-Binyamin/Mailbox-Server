be55fe0d2a2bd977dbde19e7ff6b4eae4b388848
AviadDerli <aviad@workin.co.il>
2024-04-10 12:37:27 +0300

    auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,6 +1,9 @@
 
 async function auth(req,res,next){
     try{
+        // חילוץ מהבקשה >> Autherization Bearer
+        // בדיקת תקינות טוקן
+        // בדיקה מול הדאטה-בייס
             req.body.user={_id:"660d66c5f3e17a763868202e"}
             next()
     }
